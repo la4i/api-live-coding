ae5b5da2dd5187d816d00949304582f74d212c9f
*la4i* <*vlad_f_1723@mail.ru*>
2023-06-14 11:58:31 +0300

    Оживлчяем форму

--- api.js
+++ api.js
@@ -41,7 +41,7 @@ export function addTodo({ text, token }) {
 }
 
 // https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
-export function login({ login, password }) {
+export function loginUser({ login, password }) {
   return fetch("https://webdev-hw-api.vercel.app/api/user/login", {
     method: "POST",
     body: JSON.stringify({
@@ -49,6 +49,9 @@ export function login({ login, password }) {
       password,
     }),
   }).then((response) => {
+    if (response.status === 400) {
+      throw new Error("Неверный логин или пароль");
+    }
     return response.json();
   });
 }
